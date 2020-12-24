ee2937463ab846ea076e9f9b0cc23a6c1df300b3
Brandon <tangbt@uci.edu>
2020-12-23 19:10:31 -0800

    Added room names to lobby page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-//pages
-import HomePage from "./pages/HomePage";
-import LobbyPage from "./pages/LobbyPage";
-import RoomPage from "./pages/RoomPage";
-import GamePage from "./pages/GamePage";
-
-//context providers
-import { PlayerProvider } from "./context/player";
-
-function App() {
-	return (
-		<div>
-			<Router>
-				<Switch>
-					<Route path="/" exact component={HomePage} />
-					<Route path="/lobby" render={() => <LobbyPage />} />
-					<Route
-						path="/room/:id"
-						render={({ match }) => <RoomPage gameId={match.params.id} />}
-					/>
-					<Route
-						path="/game/:id"
-						render={({ match }) => <GamePage gameId={match.params.id} />}
-					/>
-				</Switch>
-			</Router>
-		</div>
-	);
-}
-
-export default App;
-=======
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -70,4 +34,3 @@ function App() {
 }
 
 export default App;
->>>>>>> main
--- src/pages/LobbyPage.js
+++ src/pages/LobbyPage.js
@@ -7,52 +7,60 @@ import "firebase/firestore";
 import "firebase/database";
 import "firebase/auth";
 
-
 const firebaseConfig = {
-	apiKey: "AIzaSyCi1DIq90GIhBHDoQNQp5jAA5DqgmyQnLc",
-	authDomain: "browserparty.firebaseapp.com",
-	projectId: "browserparty",
-	storageBucket: "browserparty.appspot.com",
-	messagingSenderId: "477711712483",
-	appId: "1:477711712483:web:bf4c204e171208b9a445d6",
-	measurementId: "G-TBF684DB8P",
+  apiKey: "AIzaSyCi1DIq90GIhBHDoQNQp5jAA5DqgmyQnLc",
+  authDomain: "browserparty.firebaseapp.com",
+  projectId: "browserparty",
+  storageBucket: "browserparty.appspot.com",
+  messagingSenderId: "477711712483",
+  appId: "1:477711712483:web:bf4c204e171208b9a445d6",
+  measurementId: "G-TBF684DB8P",
 };
 
 if (!firebase.apps.length) {
-	firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 } else {
-	firebase.app(); // if already initialized, use that one
+  firebase.app(); // if already initialized, use that one
 }
 
-//const firestore = firebase.firestore();
+const firestore = firebase.firestore();
 
 function LobbyPage() {
-	const [redirect, setRedirect] = useState(false);
-	const [rooms, setRooms] = useState([]);
-		//const gamesRef = firestore.collection("games");
+  const [redirect, setRedirect] = useState(false);
+  const [rooms, setRooms] = useState([]);
+  //const gamesRef = firestore.collection("games");
+  let db = firebase.firestore();
+  //real time listener
+  useEffect(() => {
+    setRooms([]);
+    let unsubscribe = db
+      .collection("games")
+      .where("status", "==", "waiting")
+      .onSnapshot((snapshot) => {
+        let changes = snapshot.docChanges();
+        changes.forEach((change) => {
+          //setRooms(change.doc.id);
+          setRooms((rooms) => [rooms, change.doc.id]);
+        });
+      });
+    return () => unsubscribe();
+  }, []);
 
-	//  useEffect(() => {
-	// 	 db.collection('games').onSnapshot(snapshot => (
-	// 		 setRooms(snapshot.docs.map(doc => doc.data()))
-	// 		 ))
-	//  }, [])
+  if (redirect) {
+    return <Redirect to={redirect} push />;
+  }
 
-
-	if (redirect) {
-		return <Redirect to={redirect} push />;
-	}
-
-	return (
-		<div>
-			<h1>BrowserParty</h1>
-			<button onClick={() => setRedirect("/room/" + generate().dashed)}>
-				New Room
-			</button>
-			<h2>Existing Rooms</h2>
-			<p>{rooms}</p>
-			<button onClick={() => setRedirect("/room/kangaroo")}>Join</button>
-		</div>
-	);
+  return (
+    <div>
+      <h1>BrowserParty</h1>
+      <button onClick={() => setRedirect("/room/" + generate().dashed)}>
+        New Room
+      </button>
+      <h2>Existing Rooms</h2>
+      <p>{rooms}</p>
+      <button onClick={() => setRedirect("/room/kangaroo")}>Join</button>
+    </div>
+  );
 }
 
 export default LobbyPage;
