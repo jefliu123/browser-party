831c3716c274a2170c86b3ca326d9f3b8ca551a0
dmarke34 <aaron.aparicio34@gmail.com>
2020-12-28 21:54:23 -0800

    Implemented homepage rough sketch

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
+import Carousel from 'react-bootstrap/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import skribblio from '../images/skribblio.png';
+import tetris from '../images/tetris.jpg';
+import typeracer from '../images/typeracer.png';
 
 function HomePage() {
 	const [redirect, setRedirect] = useState(false);
@@ -9,10 +17,35 @@ function HomePage() {
 	}
 
 	return (
+	<Container fluid id = "background">
 		<div>
-			<h1>Welcome to BrowserParty</h1>
-			<button onClick={() => setRedirect(true)}>Get Started</button>
+			<h1 class = "welcome">Welcome to BrowserParty</h1>	
 		</div>
+		<Button id = "button" onClick={() => setRedirect(true)}>Get Started</Button>
+		<Carousel interval={3500}>
+		<Carousel.Item>
+		  <img  
+			className="d-block w-100"
+			src={tetris}
+			alt="First slide"
+		  />
+		</Carousel.Item>
+		<Carousel.Item>
+		  <img 
+			className="d-block w-100"
+			src={skribblio}
+			alt="Second slide"
+		  />
+		</Carousel.Item>
+		<Carousel.Item>
+		  <img 
+			className="d-block w-100"
+			src={typeracer}
+			alt="Third slide"
+		  />
+		</Carousel.Item>
+	  </Carousel>
+	</Container>
 	);
 }
 
