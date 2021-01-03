8b3cd7766f30b549bfff1f0cd0512a35a0d499c6
Jeffrey Liu <jeffreyliu66@gmail.com>
2021-01-03 06:25:09 -0800

    finished ActionSideBar and also added minigames to GamePage

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 
+//assets
+import star from "../assets/star.png";
+
 //material-ui
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import gameBoard from "../assets/gameBoard.png";
-//import gameBoardTwo from "../assets/gameBoardTwo.png";
+import gameBoardTwo from "../assets/gameBoardTwo.png";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -17,39 +20,67 @@ const useStyles = makeStyles((theme) => ({
 		backgroundImage: `url(${gameBoard})`,
 		borderRadius: 5,
 		borderStyle: "solid",
-		borderColor: "#525252",
+		borderColor: "#7f3300",
 	},
 	paper: {
+		boxShadow: "none",
+		borderRadius: 25,
 		padding: theme.spacing(2),
 		textAlign: "center",
 		minHeight: 70,
 	},
 	blueSquare: {
+		boxShadow: "none",
+		borderRadius: 25,
 		padding: theme.spacing(2),
 		textAlign: "center",
 		backgroundColor: "#6cd0f1",
 		minHeight: 70,
 	},
 	redSquare: {
+		boxShadow: "none",
+		borderRadius: 25,
 		padding: theme.spacing(2),
 		textAlign: "center",
 		backgroundColor: "#e26868",
 		minHeight: 70,
 	},
 	battleSquare: {
+		boxShadow: "none",
+		borderRadius: 25,
 		padding: theme.spacing(2),
 		textAlign: "center",
 		backgroundColor: "orange",
 		minHeight: 70,
 	},
+	purpleTeleportSquare: {
+		boxShadow: "none",
+		borderRadius: 25,
+		padding: theme.spacing(2),
+		textAlign: "center",
+		backgroundColor: "purple",
+		minHeight: 70,
+	},
+	darkGreenTeleportSquare: {
+		boxShadow: "none",
+		borderRadius: 25,
+		padding: theme.spacing(2),
+		textAlign: "center",
+		backgroundColor: "#2c5a2c",
+		minHeight: 70,
+	},
 	starSquare: {
+		boxShadow: "none",
+		borderRadius: 25,
 		padding: theme.spacing(2),
 		textAlign: "center",
 		color: "black",
-		backgroundColor: "yellow",
+		backgroundColor: "transparent",
 		minHeight: 70,
 	},
 	bridgeSquare: {
+		boxShadow: "none",
+		borderRadius: 25,
 		padding: theme.spacing(2),
 		textAlign: "center",
 		backgroundColor: "#9c5d30",
@@ -89,352 +120,330 @@ function GameBoard({ game }) {
 				<Grid item xs={2}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.redSquare}>
-						9 <br></br>
 						{displayPlayerNamesAtPosition(9)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						10 <br></br>
 						{displayPlayerNamesAtPosition(10)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.battleSquare}>
-						11 <br></br>
 						{displayPlayerNamesAtPosition(11)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						12 <br></br>
 						{displayPlayerNamesAtPosition(12)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}></Grid>
 				<Grid item xs={1}>
-					{/* <Paper className={classes.starSquare}>
-						Star <br></br>
-					</Paper> */}
+					{game.gameData.starPos === 30 && (
+						<Paper className={classes.starSquare}>
+							<img
+								src={star}
+								alt="star"
+								style={{ maxWidth: "100%", height: "auto" }}
+							/>
+						</Paper>
+					)}
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						30 <br></br>
 						{displayPlayerNamesAtPosition(30)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.paper}>
-						29 <br></br>
+					<Paper className={classes.darkGreenTeleportSquare}>
 						{displayPlayerNamesAtPosition(29)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						28 <br></br>
 						{displayPlayerNamesAtPosition(28)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						27 <br></br>
 						{displayPlayerNamesAtPosition(27)}
 					</Paper>
 				</Grid>
 				{/* ROW 2 */}
 				<Grid item xs={1}>
-					{/* <Paper className={classes.starSquare}>
-						Star <br></br>
-					</Paper> */}
+					{game.gameData.starPos === 5 && (
+						<Paper className={classes.starSquare}>
+							<img
+								src={star}
+								alt="star"
+								style={{ maxWidth: "100%", height: "auto" }}
+							/>
+						</Paper>
+					)}
 				</Grid>
 				<Grid item xs={1}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						8 <br></br>
 						{displayPlayerNamesAtPosition(8)}
 					</Paper>
 				</Grid>
 				<Grid item xs={2}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.redSquare}>
-						13 <br></br>
 						{displayPlayerNamesAtPosition(13)}
 					</Paper>
 				</Grid>
 				<Grid item xs={2}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						31 <br></br>
 						{displayPlayerNamesAtPosition(31)}
 					</Paper>
 				</Grid>
 				<Grid item xs={2}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.battleSquare}>
-						26 <br></br>
 						{displayPlayerNamesAtPosition(26)}
 					</Paper>
 				</Grid>
 				{/* ROW 3 */}
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						5 <br></br>
 						{displayPlayerNamesAtPosition(5)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.battleSquare}>
-						6 <br></br>
 						{displayPlayerNamesAtPosition(6)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						7 <br></br>
 						{displayPlayerNamesAtPosition(7)}
 					</Paper>
 				</Grid>
 				<Grid item xs={2}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						14 <br></br>
 						{displayPlayerNamesAtPosition(14)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.bridgeSquare}></Paper>
+					{/* <Paper className={classes.bridgeSquare}></Paper> */}
 				</Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.bridgeSquare}></Paper>
+					{/* <Paper className={classes.bridgeSquare}></Paper> */}
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						22 <br></br>
 						{displayPlayerNamesAtPosition(22)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						23 <br></br>
 						{displayPlayerNamesAtPosition(23)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.redSquare}>
-						24 <br></br>
 						{displayPlayerNamesAtPosition(24)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						25 <br></br>
 						{displayPlayerNamesAtPosition(25)}
 					</Paper>
 				</Grid>
 				{/* ROW 4 */}
 				<Grid item xs={1}>
 					<Paper className={classes.redSquare}>
-						4 <br></br>
 						{displayPlayerNamesAtPosition(4)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}></Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.bridgeSquare}></Paper>
+					{/* <Paper className={classes.bridgeSquare}></Paper> */}
 				</Grid>
 				<Grid item xs={2}></Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.bridgeSquare}></Paper>
+					{/* <Paper className={classes.bridgeSquare}></Paper> */}
 				</Grid>
 				<Grid item xs={5}></Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.bridgeSquare}></Paper>
+					{/* <Paper className={classes.bridgeSquare}></Paper> */}
 				</Grid>
 				{/* ROW 5 */}
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						3 <br></br>
 						{displayPlayerNamesAtPosition(3)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.bridgeSquare}></Paper>
+					{/* <Paper className={classes.bridgeSquare}></Paper> */}
 				</Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.bridgeSquare}></Paper>
+					{/* <Paper className={classes.bridgeSquare}></Paper> */}
 				</Grid>
 				<Grid item xs={2}></Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.bridgeSquare}></Paper>
+					{/* <Paper className={classes.bridgeSquare}></Paper> */}
 				</Grid>
 				<Grid item xs={1}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						42 <br></br>
 						{displayPlayerNamesAtPosition(42)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						43 <br></br>
 						{displayPlayerNamesAtPosition(43)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.redSquare}>
-						44 <br></br>
 						{displayPlayerNamesAtPosition(44)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						45 <br></br>
 						{displayPlayerNamesAtPosition(45)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						32 <br></br>
 						{displayPlayerNamesAtPosition(32)}
 					</Paper>
 				</Grid>
 				{/* ROW 6 */}
 				<Grid item xs={1}>
-					<Paper className={classes.paper}>
-						2 <br></br>
+					<Paper className={classes.purpleTeleportSquare}>
 						{displayPlayerNamesAtPosition(2)}
 					</Paper>
 				</Grid>
 				<Grid item xs={2}></Grid>
 				<Grid item xs={1}>
-					{/* <Paper className={classes.starSquare}>
-						Star <br></br>
-					</Paper> */}
+					{game.gameData.starPos === 19 && (
+						<Paper className={classes.starSquare}>
+							<img
+								src={star}
+								alt="star"
+								style={{ maxWidth: "100%", height: "auto" }}
+							/>
+						</Paper>
+					)}
 				</Grid>
 				<Grid item xs={1}></Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.bridgeSquare}></Paper>
+					{/* <Paper className={classes.bridgeSquare}></Paper> */}
 				</Grid>
 				<Grid item xs={1}></Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.paper}>
-						41 <br></br>
+					<Paper className={classes.purpleTeleportSquare}>
 						{displayPlayerNamesAtPosition(41)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}></Grid>
 				<Grid item xs={1}>
-					{/* <Paper className={classes.starSquare}>
-						Star <br></br>
-					</Paper> */}
+					{game.gameData.starPos === 44 && (
+						<Paper className={classes.starSquare}>
+							<img
+								src={star}
+								alt="star"
+								style={{ maxWidth: "100%", height: "auto" }}
+							/>
+						</Paper>
+					)}
 				</Grid>
 				<Grid item xs={1}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.redSquare}>
-						33 <br></br>
 						{displayPlayerNamesAtPosition(33)}
 					</Paper>
 				</Grid>
 				{/* ROW 7 */}
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						1 <br></br>
 						{displayPlayerNamesAtPosition(1)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						21 <br></br>
 						{displayPlayerNamesAtPosition(21)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.redSquare}>
-						20 <br></br>
 						{displayPlayerNamesAtPosition(20)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						19 <br></br>
 						{displayPlayerNamesAtPosition(19)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.battleSquare}>
-						15 <br></br>
 						{displayPlayerNamesAtPosition(15)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						40 <br></br>
 						{displayPlayerNamesAtPosition(40)}
 					</Paper>
 				</Grid>
 				<Grid item xs={3}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						34 <br></br>
 						{displayPlayerNamesAtPosition(34)}
 					</Paper>
 				</Grid>
 				{/* ROW 8 */}
 				<Grid item xs={1}>
 					<Paper className={classes.bridgeSquare}>
-						0 <br></br>
 						{displayPlayerNamesAtPosition(0)}
 					</Paper>
 				</Grid>
 				<Grid item xs={2}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						18 <br></br>
 						{displayPlayerNamesAtPosition(18)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
-					<Paper className={classes.paper}>
-						17 <br></br>
+					<Paper className={classes.darkGreenTeleportSquare}>
 						{displayPlayerNamesAtPosition(17)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						16 <br></br>
 						{displayPlayerNamesAtPosition(16)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}></Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.redSquare}>
-						39 <br></br>
 						{displayPlayerNamesAtPosition(39)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						38 <br></br>
 						{displayPlayerNamesAtPosition(38)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						37 <br></br>
 						{displayPlayerNamesAtPosition(37)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.battleSquare}>
-						36 <br></br>
 						{displayPlayerNamesAtPosition(36)}
 					</Paper>
 				</Grid>
 				<Grid item xs={1}>
 					<Paper className={classes.blueSquare}>
-						35 <br></br>
 						{displayPlayerNamesAtPosition(35)}
 					</Paper>
 				</Grid>
--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -19,6 +19,7 @@ import ActionSideBar from "../components/ActionSideBar";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import Modal from "@material-ui/core/Modal";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyCi1DIq90GIhBHDoQNQp5jAA5DqgmyQnLc",
@@ -41,6 +42,21 @@ const firestore = firebase.firestore();
 const useStyles = makeStyles((theme) => ({
 	container: {
 		height: "100%",
+		fontFamily: "cursive",
+	},
+	modal: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	minigameBox: {
+		fontFamily: "cursive",
+		outline: 0,
+		marginTop: 10,
+		padding: 20,
+		textAlign: "center",
+		minHeight: "70vh",
+		minWidth: "50vw",
 	},
 	playerBar: {
 		textAlign: "center",
@@ -84,11 +100,244 @@ function GamePage({ gameId }) {
 		}
 	}, [game]);
 
+	function renderMinigameRules(minigameId) {
+		switch (minigameId) {
+			case 0:
+				return (
+					<div className="minigameRules">
+						<button className="getStartedButton" onClick={rerollMinigame}>
+							REROLL
+						</button>
+						<h1>Game: Scribbl</h1>
+						<h2>
+							<a
+								style={{ color: "white" }}
+								href="https://skribbl.io/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								https://skribbl.io/
+							</a>{" "}
+							<br></br>
+							Create private room -><br></br>
+							Share link<br></br>
+							Rounds: 1<br></br>
+							Draw time: 30 seconds<br></br>
+						</h2>
+						<h1>TOP 3 WIN</h1>
+					</div>
+				);
+			case 1:
+				return (
+					<div className="minigameRules">
+						<button className="getStartedButton" onClick={rerollMinigame}>
+							REROLL
+						</button>
+						<h1>Game: Tetris</h1>
+						<h2>
+							<a
+								style={{ color: "white" }}
+								href="https://jstris.jezevec10.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								https://jstris.jezevec10.com/
+							</a>{" "}
+							<br></br>
+							Lobby -> Create Room -><br></br>
+							Share link<br></br>
+							Private: check<br></br>
+							Presets: default<br></br>
+						</h2>
+						<h1>TOP 3 WIN</h1>
+					</div>
+				);
+			case 2:
+				return (
+					<div className="minigameRules">
+						<button className="getStartedButton" onClick={rerollMinigame}>
+							REROLL
+						</button>
+						<h1>Game: Typeracer</h1>
+						<h2>
+							<a
+								style={{ color: "white" }}
+								href="https://play.typeracer.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								https://play.typeracer.com/
+							</a>{" "}
+							<br></br>
+							Race Your Friends -><br></br>
+							Share link<br></br>
+						</h2>
+						<h1>TOP 3 WIN</h1>
+					</div>
+				);
+			case 3:
+				return (
+					<div className="minigameRules">
+						<button className="getStartedButton" onClick={rerollMinigame}>
+							REROLL
+						</button>
+						<h1>Game: Codenames</h1>
+						<h2>
+							<a
+								style={{ color: "white" }}
+								href="https://codies.xyz/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								https://codies.xyz/
+							</a>{" "}
+							<br></br>
+							Enter name, password -> Create New Game -><br></br>
+							Share link<br></br>
+							Randomize teams once<br></br>
+							30 second timer if necessary<br></br>
+						</h2>
+						<h1>WINNING TEAM WINS</h1>
+					</div>
+				);
+			case 4:
+				return (
+					<div className="minigameRules">
+						<button className="getStartedButton" onClick={rerollMinigame}>
+							REROLL
+						</button>
+						<h1>Game: Spyfall</h1>
+						<h2>
+							<a
+								style={{ color: "white" }}
+								href="https://spyfall.adrianocola.com/#"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								https://spyfall.adrianocola.com/#
+							</a>{" "}
+							<br></br>
+							Create Room -><br></br>
+							Share link<br></br>
+							Timer: 6:00<br></br>
+						</h2>
+						<h1>SPY OR EVERYONE ELSE WINS</h1>
+					</div>
+				);
+			case 5:
+				return (
+					<div className="minigameRules">
+						<button className="getStartedButton" onClick={rerollMinigame}>
+							REROLL
+						</button>
+						<h1>Game: Trivia</h1>
+						<h2>
+							<a
+								style={{ color: "white" }}
+								href="https://protobowl.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								https://protobowl.com/
+							</a>{" "}
+							<br></br>
+							Go to Room -><br></br>
+							Share link<br></br>
+							Difficulty: Middle School, Everything<br></br>
+							Play at least 10 questions<br></br>
+						</h2>
+						<h1>TOP 3 WIN</h1>
+					</div>
+				);
+			default:
+				return (
+					<div className="minigameRules">
+						<button className="getStartedButton" onClick={rerollMinigame}>
+							REROLL
+						</button>
+						<h1>ERROR PLEASE REROLL</h1>
+					</div>
+				);
+		}
+	}
+
+	function rerollMinigame() {
+		firestore
+			.collection("games")
+			.doc(`${gameId}`)
+			.update({
+				"gameData.minigame": Math.floor(Math.random() * 6),
+			});
+	}
+
+	function winMinigame() {
+		firestore
+			.collection("games")
+			.doc(`${gameId}`)
+			.update({
+				[`gameData.players.${name}.coins`]: firebase.firestore.FieldValue.increment(
+					10
+				),
+			});
+	}
+
+	function revertWinMinigame() {
+		firestore
+			.collection("games")
+			.doc(`${gameId}`)
+			.update({
+				[`gameData.players.${name}.coins`]: firebase.firestore.FieldValue.increment(
+					-10
+				),
+			});
+	}
+
+	function endMinigamePhase() {
+		firestore
+			.collection("games")
+			.doc(`${gameId}`)
+			.update({
+				"gameData.phase": "rolling",
+				"gameData.minigame": Math.floor(Math.random() * 6),
+			});
+	}
+
 	return (
 		<div className="gamePage">
 			<div id="cloud-intro">
 				{game && game.status === "ingame" ? (
 					<Grid container spacing={0} className={classes.container}>
+						<Modal
+							className={classes.modal}
+							open={game.gameData.phase === "minigame"}
+						>
+							<Paper className={classes.minigameBox}>
+								<h1>Minigame Time!</h1>
+								{renderMinigameRules(game.gameData.minigame)}
+								<div>
+									<button className="getStartedButton" onClick={winMinigame}>
+										I WON
+									</button>
+									<span> </span>
+									<button
+										className="getStartedButton"
+										onClick={revertWinMinigame}
+									>
+										NVM
+									</button>
+									<span> </span>
+									<button
+										className="getStartedButton"
+										onClick={endMinigamePhase}
+									>
+										FINISH
+									</button>
+								</div>
+								<h2>
+									(ONLY CLICK FINISH AFTER ALL WINNERS HAVE RECEIVED REWARDS)
+								</h2>
+							</Paper>
+						</Modal>
 						<Grid item xs={1}></Grid>
 						<Grid item xs={10} className={classes.playerBar}>
 							<PlayerBar game={game} currentPlayer={currentPlayer} />
@@ -96,7 +345,7 @@ function GamePage({ gameId }) {
 						<Grid item xs={1}></Grid>
 						<Grid item xs={1}></Grid>
 						<Grid item xs={7}>
-							<Paper className={classes.gameBoard} elevation={10}>
+							<Paper className={classes.gameBoard} elevation={24}>
 								<GameBoard game={game} />
 							</Paper>
 						</Grid>
@@ -110,8 +359,8 @@ function GamePage({ gameId }) {
 						<Grid item xs={1}></Grid>
 					</Grid>
 				) : (
-					<div style={{ "text-align": "center", height: "100%" }}>
-						<h1 style={{ "padding-top": "100px", "padding-bottom": "100px" }}>
+					<div style={{ textAlign: "center", height: "100%" }}>
+						<h1 style={{ paddingTop: "100px", paddingBottom: "100px" }}>
 							No active game found.
 						</h1>
 					</div>
